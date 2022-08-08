c75f95eafb0b6d5622a089716190e702e40ab4b3
ROBIND KUMAR <kumarrobind386@gmail.com>
2022-08-09 04:12:59 +0530

    ok

--- js/inbox.js
+++ js/inbox.js
@@ -1,5 +1,8 @@
-/* we will give margin to the food from Math.random() function of javascript
+/* Method::1 - we will give margin to the food from Math.random() function of javascript
     we will generate two variable one for margin-left and other for margin-top  */
+
+/* Method::2 - Even this can be done using grid system, where we will add different boxes for food & snake ,
+ if food is in same grid as snake then it will initiate another event  */
 
 function foodPlace(){
     let food = document.getElementById('food');
