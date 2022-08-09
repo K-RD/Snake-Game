7398e674318990ffce0a5480608798862a93d386
Robind Kumar <kumarrobind386@gmail.com>
2022-08-09 11:46:03 +0530

    now

--- js/inbox.js
+++ js/inbox.js
@@ -3,13 +3,32 @@
 
 /* Method::2 - Even this can be done using grid system, where we will add different boxes for food & snake ,
  if food is in same grid as snake then it will initiate another event  */
+let board = document.getElementById("board");
+let boardHeight = board.
+
 
 function foodPlace(){
     let food = document.getElementById('food');
-    let foodMarginLeft = math.random()* 50;
-    let foodMarginTop = math.random()* 50;
-    food.style.marginLeft=foodMarginLeft;
-    food.style.marginTop=foodMarginTop;
+    let foodMarginLeft = Math.random()* 730;
+    let foodMarginTop = Math.random()* 490;
+    food.style.marginLeft=(foodMarginLeft)+"px";
+    food.style.marginTop=(foodMarginTop)+"px";
+}
+foodPlace();
+// window.onload = foodPlace();
+
+function snake_move(){
+    let snakeBody = document.getElementById("snake");
+
+    // console.log(snakeBody.innerText.length);
+    // document.write(snakeBody);
+    // let initial=[]
+}
+// snake_move();
+
+function increase_snake_length(){
+    let snakeLength = document.getElementById("snake");
+    console.log(snakeLength.style("width"));
 }
 
-window.onload = foodPlace();
+increase_snake_length();
